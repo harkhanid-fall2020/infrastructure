64d0c1fa07577f338f7f633246909914e13a62c7
Dharmik Harkhani <harkhani.d@northeastern.edu>
2020-10-23 04:42:55 -0400

    changes

--- 01/main.tf
+++ 01/main.tf
@@ -311,8 +311,19 @@ resource "aws_instance" "ec2webapp" {
   }
   iam_instance_profile = aws_iam_instance_profile.ec2_s3profile.id
   key_name = var.ec2_config.key_name
-  user_data = "${file("${path.module}/userdata.sh")}"
+  user_data = <<EOF
+#!/bin/bash
+sudo chmod 777 /etc/environment
+sudo echo 'db_user="webapp_database"' >> /etc/environment
+sudo echo 'db_username="${ var.rds_config.username}"' >> /etc/environment
+sudo echo 'db_password="${var.rds_config.password}"' >> /etc/environment
+sudo echo 'db_name="${var.rds_config.name}"' >> /etc/environment
+sudo echo 'db_host="${aws_db_instance.default.endpoint }"' >> /etc/environment
+sudo echo 's3_bucket="${var.s3_vars.bucket }"' >> /etc/environment
+sudo echo 's3_region="${var.region }"' >> /etc/environment
+EOF
 }
+
 
 # dynamoDB table
 resource "aws_dynamodb_table" "demoCheck" {
